c5c87c6daee5dc47b3b4e6b94767d789e7dbd2ac
saifulislammunna <saifulmunna28@gmail.com>
2021-09-29 19:05:20 +0600

    router added

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Home from './components/Home/Home';
 import Friends from './components/Friends/Friends';
 import About from './components/About/About';
 import NotFound from './components/NotFound/NotFound';
+import { BrowserRouter } from 'react-router-dom';
  
 
 function App() {
   return (
     <div className="App">
-       <Friends></Friends>
+       <BrowserRouter>
+       </BrowserRouter>
         
     </div>
   );
